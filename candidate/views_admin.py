05032a9fec5f21db633f055b5b7e8f18ed4b437f
Kateryna Stetsenko <itcreativeusa@gmail.com>
2024-11-06 08:44:12 -0800

    added '%Y%m%d'instead of DATE_FORMAT_YMD

--- candidate/views_admin.py
+++ candidate/views_admin.py
@@ -1316,7 +1316,7 @@ def candidate_list_view(request):
             # datetime_now = timezone.localize(datetime.now())
             timezone, datetime_now = generate_localized_datetime_from_obj()
             if positive_value_exists(election.election_day_text):
-                date_of_election = timezone.localize(datetime.strptime(election.election_day_text, DATE_FORMAT_YMD)) # "%Y-%m-%d"
+                date_of_election = timezone.localize(datetime.strptime(election.election_day_text, '%Y%m%d')) # "%Y-%m-%d"
                 if date_of_election > datetime_now:
                     time_until_election = date_of_election - datetime_now
                     election.days_until_election = convert_to_int(DATE_FORMAT_DAY_TWO_DIGIT % time_until_election.days) # "%d"
