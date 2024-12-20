3ee206980046c79b56c81d4f6f70e2e2318859e3
stevepodell <steve@podell.com>
2024-12-20 11:39:20 -0800

    Fastload: fixes to displayed timing issues Fixed python manage.py create_dev_user so that it works from the command line again (the problem was due to a major breaking change in a library) Did not change the environment_variables_template.json, but I can't see how a dev could create a lower case WeVoteServerDB, if they were following the instructions.  I'll rerun from scratch at some point in the future, I think the instructions can be further simplified.

--- wevote_social/management/commands/create_dev_user.py
+++ wevote_social/management/commands/create_dev_user.py
@@ -6,7 +6,7 @@ from voter.models import VoterManager
 class Command(BaseCommand):
     help = 'Creates an initial user for you (the developer) without setting up oAuth on this local WeVoteServer.'
 
-    requires_system_checks = False
+    requires_system_checks = [False]
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -19,8 +19,8 @@ class Command(BaseCommand):
             print("Usage: $ python manage.py create_dev_user first_name last_name email password")
             print("Example: $ python manage.py create_dev_user Dianne Feinstein senator@feinstein.senate.gov DiFiPass")
         else:
-            print("Creating developer first name=" +  args[0] + ", last name=" + args[1] + ", email=" + args[2])
+            print("Creating developer first name=" +  args[0] + ", last name=" + args[1] + ", email=" + args[2] +
+                  ", password =" + args[3])
             VoterManager().create_developer(args[0], args[1], args[2], args[3])
-            # create_developer(self, first_name, last_name, email, password
 
         print("End of create_dev_user")
